fa337203c64f87b8b8fd08327b849a2bab35acc5
firoj10 <mdfirojhasan.info@gmail.com>
2023-05-21 12:27:50 +0600

    added details product

--- src/pages/Reviews/Reviews.jsx
+++ src/pages/Reviews/Reviews.jsx
@@ -9,7 +9,7 @@ const Reviews = () => {
     <img src={img} className="max-auto rounded-lg shadow-2xl" />
     <div >
       <h1 className="text-5xl font-bold text-orange-900">Guardians of the Galaxy Vol. Dhaka and  Happy Meal Toys Now Available at McDonald’s</h1>
-      <p className="py-6">Each Happy Meal box includes one of eight super hero toys based on characters from the movie, and will be available for a limited time at participating restaurants nationwide, while supplies last..</p>
+      <p className="py-6">Each Happy Meal box includes one of eight super hero toys based on characters from the super hero toys baby toys  and will be available for a limited time at participating restaurants nationwide, while supplies last..</p>
       <button className=" text-white p-3 rounded bg-orange-900">View Details</button>
     </div>
   </div>
